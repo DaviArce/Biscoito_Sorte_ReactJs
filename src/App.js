1371216d4fa2390b3eaade04aaaff305d97248a5
daviArce <daviarce_oliveira@hotmail.com>
2020-11-27 21:19:40 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./estilo.css";
+
+import Phrase from "./components/Phrase";
+import Image from "./components/Image";
+import Button from "./components/Button";
 
 function App() {
+  const image = require("./assets/biscoito.png");
+  const phrase = [
+    { id: 1, text: "diringulendiu" },
+    { id: 2, text: "muito mais interessante" },
+    { id: 3, text: "meu pai" },
+    { id: 4, text: "asdlaasdas" },
+    { id: 5, text: "gkndsafsfent" },
+  ];
+  const [initial,setInitial] = useState(`"Abra o biscoito"`);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Image image={image}/>
+      <Phrase phrase={initial} />
+      <Button do={(e)=>{
+        const value = Math.floor((Math.random()* (0+5)-0));
+        console.log(value);
+        setInitial(phrase[value]["text"]);
+        e.preventDefault();
+      }}>Aperte</Button>
+      
     </div>
   );
 }
---
+++ src/components/Phrase/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Phrase(props){
+  return(
+    <div>
+      <h3 className="Phrase">{props.phrase}</h3>
+    </div>
+  );
+}
